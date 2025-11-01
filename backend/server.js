199382ebffc17e8c319b69f4f94b17058b1511f3
Imankalyan99 <imanmaiti331@gmail.com>
2025-11-01 16:27:20 +0530

    hello

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,35 @@
+// const express = require('express');
+// const { PassThrough } = require('stream');
+// const app = express();
+// const port = process.env.port || 5000;
+
+// // data Pass
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
 const express = require('express');
-const { PassThrough } = require('stream');
 const app = express();
-const port = process.env.port || 5000;
+const dotenv = require('dotenv')
 
-// data Pass
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const databaseConnect = require('./config/database')
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+dotenv.config({
+     path : 'backend/config/config.env'
+})
+
+const PORT = process.env.PORT || 5000
+app.get('/', (req, res)=>{
+     res.send('This is from backend Sever')
+})
+
+databaseConnect();
+
+app.listen(PORT, ()=>{
+     console.log(`Server is running on port ${PORT}`)
+})
